f15a21d00ca0b99b42eb9bec843af0811ff482a7
ouima <>
2021-06-04 18:19:23 +0000

    Shortened the space between the number and the question

--- candidate-testing.js
+++ candidate-testing.js
@@ -9,7 +9,7 @@ let candidateName = "";
 let question = "Who was the first American woman in space? ";
 let correctAnswer = "Sally Ride";
 let candidateAnswer = "";  
-let questions = ["Who was the first American woman in space? ", "True or false: 5 kilometer == 5000 meters? ", "(5 + 3)/2 * 10 = ? ", "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ", "What is the minimum crew size for the ISS? "];
+let questions = ["Who was the first American woman in space? ", "True or false: 5 kilometer == 5000 meters? ", "(5 + 3)/2 * 10 = ? ", "Given the array [8, 'Orbit','Trajectory', 45], what entry is at index 2? ", "What is the minimum crew size for the ISS? "];
 let correctAnswers = ["Sally Ride", "true", "40", 	"Trajectory", "3"];
 let candidateAnswers = [];
 
@@ -22,7 +22,7 @@ candidateName = input.question("Candidate name? ");
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
   for (let i = 0; i < correctAnswers.length; i++){
-      let numberingSystem = i + 1 + ") " +" "; 
+      let numberingSystem = i + 1 + ") "; 
       candidateAnswers[i] = input.question(numberingSystem + questions[i]);
       console.log(`Your answer: ${candidateAnswers[i]} \nCorrect Answer: ${correctAnswers[i]} \n`);
        
